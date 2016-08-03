8d89ff08f1fe236a7e1cd9828bb81c04d3aa78df
Cole Brumley <cole.brumley@gmail.com>
2016-08-03 00:23:41 -0500

    fun with mock

--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -2,6 +2,7 @@
 
 from unittest import TestCase
 import des
+import mock
 
 class TestWatcher(TestCase):
     def setUp(self):
@@ -9,3 +10,17 @@ class TestWatcher(TestCase):
 
     def test_create(self):
         self.assertTrue(isinstance(self.test_watcher, des.watcher.Watcher))
+
+    def test_watch(self):
+        event_dicts = [
+            dict(Type='container', Action='start'),
+            dict(Type='container', Action='die'),
+            dict(Type='network', Action='create'),
+            ]
+        clmock = mock.MagicMock()
+        clmock.events.return_value = iter(event_dicts)
+        self.test_watcher.client = clmock
+        action = mock.MagicMock()
+        self.test_watcher.wait_event(action)
+        for d in mock.call.call_list():
+            action.assert_called_with(d)
