6621f28aa7ca9d1aa07ce98718f8459762a10209
Cole Brumley <cole.brumley@gmail.com>
2016-08-05 22:38:26 -0500

    moar test

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -3,6 +3,7 @@
 import os
 from sys import getdefaultencoding
 from unittest import TestCase
+import mock
 
 # import mock
 from .context import des
@@ -13,10 +14,10 @@ class TestRunner(TestCase):
         self.test_runner = des.runner.ScriptRunner('/tmp')
 
     def test_create(self):
-        '''Make sure that TestRunneris in fact a TestRunner'''
+        '''Make sure that TestRunner is in fact a TestRunner'''
         self.assertTrue(isinstance(self.test_runner, des.runner.ScriptRunner))
 
-    def test_build_env(self):
+    def test_build_env_dict_translation(self):
         '''Test docker event to environment dict translation'''
         event_dict = {
             'status': 'start',
@@ -39,8 +40,30 @@ class TestRunner(TestCase):
         result = self.test_runner.build_env(event_dict)
         self.assertDictEqual(result, anticipated_result)
 
-    def test_exec(self):
-        '''Test execution'''
-        result = self.test_runner.script_exec('/usr/bin/printenv', {'ENV_VAR_1': 'env_val_1'})
-        self.assertEqual(result, bytes('ENV_VAR_1=env_val_1\nPWD='+ os.getcwd() +'\n',
-                                       getdefaultencoding()))
+    def test_script_exec_handles_good_scripts(self):
+        '''Test good executions'''
+        expected_output = bytes('ENV_VAR_1=env_val_1\nPWD='+ \
+                                os.getcwd() +'\n', getdefaultencoding())
+
+        result = self.test_runner.script_exec('/usr/bin/printenv',
+                                             {'ENV_VAR_1': 'env_val_1'})
+
+        self.assertEqual(result, expected_output)
+
+    def test_script_exec_handles_script_errors(self):
+        '''Test bad executions'''
+        self.assertIsNone(self.test_runner.script_exec('/bin/false', {}))
+
+    def test_run_handles_missing_scripts(self):
+        '''Test missing scripts'''
+        event = dict(Type='missing', Action='file')
+        self.assertIsNone(self.test_runner.run(event))
+    
+    def test_run_handles_existing_scripts(self):
+        '''Test existing scripts'''
+        self.test_runner.basedir = ''
+        event = dict(Type='bin', Action='true')
+        exec_mock = mock.MagicMock()
+        self.test_runner.script_exec = exec_mock
+        self.assertIsNone(self.test_runner.run(event))
+        exec_mock.assert_called_once_with('/bin/true', {'TYPE': 'bin', 'ACTION': 'true'})
--- tests/test_watcher.py
+++ tests/test_watcher.py
@@ -9,30 +9,29 @@ from .context import des
 
 class TestWatcher(TestCase):
     def setUp(self):
-        self.test_watcher = des.watcher.Watcher()
-
-    def test_create(self):
-        self.assertTrue(isinstance(self.test_watcher, des.watcher.Watcher))
-
-    def test_watch(self):
-        event_dicts = [
+        self.event_dicts = [
             dict(Type='container', Action='start'),
             dict(Type='container', Action='die'),
             dict(Type='network', Action='create'),
             ]
 
+        self.test_watcher = des.watcher.Watcher()
         mock_docker_client = mock.MagicMock()
-        mock_docker_client.events.return_value = iter(event_dicts)
+        mock_docker_client.events.return_value = iter(self.event_dicts)
         self.test_watcher.client = mock_docker_client
 
+    def test_create_watcher(self):
+        self.assertTrue(isinstance(self.test_watcher, des.watcher.Watcher))
+
+    def test_watch_events_passes_events_to_action(self):
         action = mock.MagicMock()
         self.test_watcher.wait_event(action)
-        action.assert_has_calls([mock.call(d) for d in event_dicts], any_order=True)
+        action.assert_has_calls([mock.call(d) for d in self.event_dicts], any_order=True)
 
-        # test failure cases
+    def test_watch_events_ensure_callable(self):
         with self.assertRaises(ValueError):
             self.test_watcher.wait_event(None)
 
-        with self.assertRaises(RuntimeError):
-            err_action = mock.MagicMock(side_effect=RuntimeError)
-            self.test_watcher.wait_event(err_action())
+    def test_watch_events_handle_runtime_error(self):
+        err_action = mock.MagicMock(side_effect=RuntimeError)
+        self.assertIsNone(self.test_watcher.wait_event(err_action))
